edd546fcbf272f348f2c6c1ef5591814cf885b1e
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2020-01-07 09:12:11 -0800

    Added gitignore and Num class

---
+++ Num.java
@@ -0,0 +1,20 @@
+public class Num  {
+    private int val;
+
+    public Num (int val)  {
+	this.val = val;
+    }
+
+    public int getVal ()  {
+	return val;
+    }
+
+    public void setVal (int val)  {
+	this.val = val;
+    }
+
+    public String toString ()  {
+	return "" + this.val;
+    }
+
+}
